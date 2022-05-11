2859c4c8fc8b12cdd314bd12d4e0df32ac3c7410
vishnugopy <vishnugoby0010@gmail.com>
2022-05-11 12:19:53 +0200

    Header style

--- App.js
+++ App.js
@@ -16,7 +16,6 @@ export default function App() {
     },
   });
 
-
   return (
     <NavigationContainer>
       <Stack.Navigator
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,70 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet , ScrollView } from "react-native";
 
-export default function HomeScreen () {
+export default function HomeScreen() {
+  const style = StyleSheet.create({
+    NoteContainer: {
+      padding: 20,
+      height: "100%",
+    },
+    Note: {
+        marginVertical: 10,
+      backgroundColor: "#fff",
+      paddingVertical: 30,
+      paddingHorizontal: 10,
+      borderRadius: 15,
+    },
+    NoteTitle: {
+      fontSize: 15,
+      fontWeight: "bold",
+      color: "#228CDB",
+      opacity: 0.7,
+      marginBottom: 10,
+    },
+    NoteContent: {
+      fontSize: 13,
+      opacity: 0.7,
+      margin: 5,
+    },
+  });
+
   return (
-    <View>
-      <Text>Welcome Home!</Text>
-    </View>
-  )
+    <ScrollView style={style.NoteContainer}>
+      <View style={style.Note}>
+        <Text style={style.NoteTitle}> Welcome Home!</Text>
+        <Text style={style.NoteContent}>
+          piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj
+          fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj
+          piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj{" "}
+        </Text>
+      </View>
+
+      <View style={style.Note}>
+        <Text style={style.NoteTitle}> Welcome Home!</Text>
+        <Text style={style.NoteContent}>
+          piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj
+          fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj
+          piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj{" "}
+        </Text>
+      </View>
+
+      <View style={style.Note}>
+        <Text style={style.NoteTitle}> Welcome Home!</Text>
+        <Text style={style.NoteContent}>
+          piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj
+          fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj
+          piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj{" "}
+        </Text>
+      </View>
+
+      <View style={style.Note}>
+        <Text style={style.NoteTitle}> Welcome Home!</Text>
+        <Text style={style.NoteContent}>
+          piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj
+          fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj
+          piodjfipj fsijsiodj piodjfipj fsijsiodj piodjfipj fsijsiodj{" "}
+        </Text>
+      </View>
+    </ScrollView>
+  );
 }

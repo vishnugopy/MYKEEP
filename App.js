a604bc1078be001993d79426b9ebf5dfeb84103f
vishnugopy <vishnugoby0010@gmail.com>
2022-05-11 11:40:01 +0200

    Pages

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import Home from "./src/screens/Home";
 import Task from "./src/screens/Task";
 import { Icon } from "react-native-elements";
@@ -10,8 +10,6 @@ import { View, Text, StyleSheet } from "react-native";
 const Stack = createStackNavigator();
 
 export default function App() {
-  const navigation = useNavigation();
-
   const style = StyleSheet.create({
     headerIcon: {
       marginRight: 10,
@@ -26,23 +24,26 @@ export default function App() {
           headerStyle: {
             backgroundColor: "#fff",
           },
+          headerTitleStyle: {
+            alignItems: "center",
+          },
           headerTintColor: "#228CDB",
         }}
       >
         <Stack.Screen
           name="Home"
           component={Home}
-          options={{
+          options={({ navigation }) => ({
             headerRight: () => (
               <Icon
                 name="plus"
                 type="feather"
                 color="#000"
                 style={style.headerIcon}
-                onPress={() => navigation.navigate('Task')}
+                onPress={() => navigation.navigate("Task")}
               />
             ),
-          }}
+          })}
         />
         <Stack.Screen name="Task" component={Task} />
       </Stack.Navigator>

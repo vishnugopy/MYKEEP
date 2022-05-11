142e29139cbfcd0eb5d3809063202a9849aa1967
vishnugopy <vishnugoby0010@gmail.com>
2022-05-11 11:12:04 +0200

    basics

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import Home from "./src/screens/Home";
+import Task from "./src/screens/Task";
+import { Icon } from "react-native-elements";
+import { View, Text, StyleSheet } from "react-native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const navigation = useNavigation();
+
+  const style = StyleSheet.create({
+    headerIcon: {
+      marginRight: 10,
+    },
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#fff",
+          },
+          headerTintColor: "#228CDB",
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerRight: () => (
+              <Icon
+                name="plus"
+                type="feather"
+                color="#000"
+                style={style.headerIcon}
+                onPress={() => navigation.navigate('Task')}
+              />
+            ),
+          }}
+        />
+        <Stack.Screen name="Task" component={Task} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Task.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Task () {
+  return (
+    <View>
+      <Text>This is New Screen</Text>
+    </View>
+  )
+}
